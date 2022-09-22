f9ed1486e800c7ff58fea4bee9bad61ce376e054
Yan Sun <tsun812@gmail.com>
2022-09-22 11:31:16 -0700

    change coordinate, cleanup

--- src/map.js
+++ src/map.js
@@ -3,7 +3,7 @@ import { useMap } from "react-leaflet/hooks";
 
 export default function Map(props) {
   return (
-    <MapContainer center={[0, 0]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

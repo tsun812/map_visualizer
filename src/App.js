7ae42ab26076948470e815ae9ec2a4954ac94160
Yan Sun <tsun812@gmail.com>
2022-09-22 12:18:28 -0700

    add leaflet css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import SearchBar from "./SearchBar";
 import LatLonData from "./latLonData";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import Map from "./map";
+import { state, setLatLon } from "./useAppState";
 function App() {
   return (
     <div className="App">
       <SearchBar />
-      <Map />
+      <div className="Map">
+        <Map />
+      </div>
     </div>
   );
 }
--- src/map.js
+++ src/map.js
@@ -3,7 +3,12 @@ import { useMap } from "react-leaflet/hooks";
 
 export default function Map(props) {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      style={{ height: "200px", width: "500px" }}
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

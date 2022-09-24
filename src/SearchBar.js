983027af6ff9134feb964b164c76314c09494c4f
Jackson <javtran@ucsc.edu>
2022-09-23 19:48:32 -0700

    added search bar styling

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -18,8 +18,10 @@ function SearchBar(props) {
         const bar = document.getElementById('address');
         if (event.target.id == 'address' || event.target.id == 'searchIcon') {
             setListHidden(true);
-            bar.style.borderRadius = '7px 7px 0 0';
-            bar.style.borderBottom= '1px solid #CFCFCF';
+            if(locList != null) {
+                bar.style.borderRadius = '7px 7px 0 0';
+                bar.style.borderBottom= '1px solid #CFCFCF';
+            }
         } else {
             setListHidden(false);
             bar.style.borderRadius = '7px';
@@ -60,26 +62,26 @@ function SearchBar(props) {
             <img src={search} alt='search icon' className='searchIcon' id='searchIcon'></img>
             <input onKeyDown={enterSearchBar} placeholder="Search location by name, address or latitude/longitude" className="searchBarInput" id='address'></input>
             <div className='locationList'>
-            {locList && listHidden &&locList.map((loc) =>
-                <div className='locationItem'>
-                    <div className='locationItemInfo'>
-                        <div className='name'>
-                            {loc.properties.name && loc.properties.name}
-                            {!loc.properties.name && 'Untitled Name'}
+                {locList && listHidden &&locList.map((loc) =>
+                    <div className='locationItem'>
+                        <div className='locationItemInfo'>
+                            <div className='name'>
+                                {loc.properties.name && loc.properties.name}
+                                {!loc.properties.name && 'Untitled Name'}
+                            </div>
+                            <div className='address'>
+                                {loc.properties.formatted}
+                            </div>
+                            <div className='latLon'>
+                                <span>Lat:</span> {loc.properties.lat}°, <span>Long:</span> {loc.properties.lon}°
+                            </div>
                         </div>
-                        <div className='address'>
-                            {loc.properties.formatted}
-                        </div>
-                        <div className='latLon'>
-                            <span>Lat:</span> {loc.properties.lat}°, <span>Long:</span> {loc.properties.lon}°
-                        </div>
+                        <button className='selectButton' onClick={() => {selectLocation(loc)}}>
+                            Select
+                        </button>
                     </div>
-                    <button className='selectButton' onClick={() => {selectLocation(loc)}}>
-                        Select
-                    </button>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
         </div>
     )
 }
